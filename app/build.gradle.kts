bd494c26fbdf21e370e506c78fa571a0e2cf9e2e
Alber <alberdsp@gmail.com>
2024-01-27 23:01:44 +0100

    agragadas dependencias gps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment:2.7.5")
     implementation("androidx.navigation:navigation-ui:2.7.5")
-    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
+    implementation("com.google.firebase:firebase-crashlytics:17.4.1")
+    implementation("com.google.android.gms:play-services-maps:17.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
